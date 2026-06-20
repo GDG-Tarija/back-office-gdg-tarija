f1ad4aae7d8e24c85c5845412b103f22ff3eafeb
pedroanze <pedro2528anze@gmail.com>
2026-06-19 21:08:59 -0400

    feat: add new authorized emails to the whitelist

--- src/app/core/config/auth-whitelist.ts
+++ src/app/core/config/auth-whitelist.ts
@@ -2,4 +2,7 @@ export const AUTH_WHITELIST: string[] = [
   'pedro2528anze@gmail.com',
   'gdgtarija@gmail.com',
   'alexander65.af@gmail.com',
+  'luisgarciabarja@gmail.com',
+  'alvarezcopa591@gmail.com',
+  'Yulenisegoviavaldez127@gmail.com',
 ];
